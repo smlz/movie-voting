b3db1fdbc5a87e47c50ece55c75eb45d02771d7f
Marco Schmalz <marco@schess.ch>
2018-06-01 12:24:48 +0200

    Reactive doughnut chart

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -1,18 +1,28 @@
 <template>
   <div class="">
     <h1>Resultat</h1>
-    <ol>
-      <li v-for="voting in votingList" :key="voting.movie">
-        {{voting.movie}} - {{voting.number}}
-      </li>
-    </ol>
+    <vue-chart type="doughnut" :data="data" :bind="true"></vue-chart>
     <router-link to="/">Vote again</router-link>
   </div>
 </template>
 
 <script>
 import Vue from 'vue'
+import VueChart from 'vue-chart'
 import db from './db'
+
+Vue.use(VueChart);
+
+
+let colors = (() => {
+  let colors = []
+  let numOfColors = 16
+  let rand = () => Math.floor(Math.random() * 255)
+  for (let i=0; i < numOfColors; i++) {
+    colors.push("rgb(" + rand() + "," + rand() + "," + rand() + ")")
+  }
+  return colors
+})()
 
 export default {
   name: 'Result',
@@ -26,15 +36,30 @@ export default {
     }
   },
   computed: {
-    votingList () {
+    data () {
       let list = []
       for (let movie in this.votings) {
         list.push({movie: movie, number: this.votings[movie]})
       }
       list.sort((v1, v2) => v1.number < v2.number)
 
-      return list
-    }
+      let labels = []
+      let backgroundColor = []
+      let data = []
+
+      for (let voting of list) {
+        labels.push(voting.movie)
+        // hash the movie name
+        let hash = voting.movie.split('').map(s => s.codePointAt(0))
+                   .reduce((a, b) => a + b)
+        backgroundColor.push(colors[hash % colors.length])
+        data.push(voting.number)
+      }
+      return {labels, datasets: [{backgroundColor, data}]}
+    },
+  },
+  components: {
+    VueChart
   }
 }
 </script>
